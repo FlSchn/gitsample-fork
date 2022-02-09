43f368e3b92ac8b42fcdd831acdd8714c53becbc
Anke Roß <anke.ross@atruvia.de>
2022-02-09 11:55:04 +0100

    2. Kommentar

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -37,6 +37,7 @@ public class Calculator {
 
 	private void setupDefaultOperations() {
 		// Es werden noch weitere Operationen folgen
+		// Ein weiterer Kommentar
 		new Equals();
 		new Minus();
 		new Plus();
